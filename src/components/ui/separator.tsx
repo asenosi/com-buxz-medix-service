c89d54205e22e0fa42d5e9947f3068386b2faad2
Aobakwe Senosi <senosiag@gmail.com>
2025-10-27 00:58:37 +0200

    Reduce border radius around cards and buttons

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+﻿import * as React from "react";
 import * as SeparatorPrimitive from "@radix-ui/react-separator";
 
 import { cn } from "@/lib/utils";
@@ -11,10 +11,11 @@ const Separator = React.forwardRef<
     ref={ref}
     decorative={decorative}
     orientation={orientation}
-    className={cn("shrink-0 bg-border", orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]", className)}
+    className={cn("shrink-0 bg-border/60", orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]", className)}
     {...props}
   />
 ));
 Separator.displayName = SeparatorPrimitive.Root.displayName;
 
 export { Separator };
+
